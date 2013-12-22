474698feebd99425a7925cdd24e8a4a3745c4406
Andrei Kuprienka <Andrei_Kuprienka@epam.com>
2013-12-22 04:26:56 +0300

    testing crud operations with page entity

--- src/Kan/CmsBundle/Controller/DefaultController.php
+++ src/Kan/CmsBundle/Controller/DefaultController.php
@@ -3,11 +3,75 @@
 namespace Kan\CmsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Kan\CmsBundle\Entity\Page;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Constraints\DateTime;
+
 
 class DefaultController extends Controller
 {
-    public function indexAction($page)
+    public function indexAction($id)
     {
-        return $this->render('KanCmsBundle:Default:index.html.twig', array('page' => $page));
+        $page = $this->getDoctrine()
+            ->getRepository('KanCmsBundle:Page')
+            ->find($id);
+
+        if (!$page) {
+            throw $this->createNotFoundException(
+                'No product found for id '. $id
+            );
+        }
+
+        return $this->render('KanCmsBundle:Default:index.html.twig', array(
+            'page' => $page,
+            'created' => $page->getCreated()->format('Y-m-d H:i:s'),
+            'edited' => $page->getEdited()->format('Y-m-d H:i:s'),
+        ));
+    }
+
+    public function createAction()
+    {
+        $page = new Page();
+        $title = 'Kan page 1';
+        $description = 'Test page description';
+        $created = $edited = new \DateTime();
+
+        $page->setTitle($title);
+        $page->setDescription($description);
+        $page->setCreated($created);
+        $page->setEdited($edited);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($page);
+        $em->flush();
+
+        return $this->render('KanCmsBundle:Default:create.html.twig', array(
+            'page' => $page,
+            'created' => $page->getCreated()->format('Y-m-d H:i:s'),
+            'edited' => $page->getEdited()->format('Y-m-d H:i:s'),
+        ));
+    }
+
+    public function updateAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $page = $em->getRepository('KanCmsBundle:Page')
+            ->find($id);
+
+        if (!$page) {
+            throw $this->createNotFoundException(
+                'No product found for id '. $id
+            );
+        }
+
+        $page->setTitle('updated title');
+
+        $em->flush();
+
+        return $this->render('KanCmsBundle:Default:update.html.twig', array(
+            'page' => $page,
+            'created' => $page->getCreated()->format('Y-m-d H:i:s'),
+            'edited' => $page->getEdited()->format('Y-m-d H:i:s'),
+        ));
     }
 }
---
+++ src/Kan/CmsBundle/Entity/Page.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace Kan\CmsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Page
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Page
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created", type="datetime")
+     */
+    private $created;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="edited", type="datetime")
+     */
+    private $edited;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Page
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Page
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     * @return Page
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime 
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set edited
+     *
+     * @param \DateTime $edited
+     * @return Page
+     */
+    public function setEdited($edited)
+    {
+        $this->edited = $edited;
+
+        return $this;
+    }
+
+    /**
+     * Get edited
+     *
+     * @return \DateTime 
+     */
+    public function getEdited()
+    {
+        return $this->edited;
+    }
+}
