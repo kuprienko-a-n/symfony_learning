74363a38daf71e50ddae3c72604792539bfe353f
Andrei Kuprienka <Andrei_Kuprienka@epam.com>
2014-01-08 16:52:33 +0300

    Quick dirty crud for pages

--- src/Kan/CmsBundle/Controller/DefaultController.php
+++ src/Kan/CmsBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Kan\CmsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Kan\CmsBundle\Entity\Page;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Validator\Constraints\DateTime;
@@ -10,7 +11,31 @@ use Symfony\Component\Validator\Constraints\DateTime;
 
 class DefaultController extends Controller
 {
-    public function indexAction($id)
+    public function indexAction()
+    {
+        $pages = $this->getDoctrine()
+            ->getRepository('KanCmsBundle:Page')
+            ->findAll();
+
+        if (!$pages) {
+            throw $this->createNotFoundException(
+                'No pages found'
+            );
+        }
+
+        foreach ($pages as &$page) {
+            $edited = $page->getEdited()->format('Y-m-d H:i:s');
+            $page->setEdited($edited);
+            $created = $page->getCreated()->format('Y-m-d H:i:s');
+            $page->setCreated($created);
+        }
+
+        return $this->render('KanCmsBundle:Default:index.html.twig', array(
+            'pages' => $pages,
+        ));
+    }
+
+    public function showAction($id)
     {
         $page = $this->getDoctrine()
             ->getRepository('KanCmsBundle:Page')
@@ -18,38 +43,62 @@ class DefaultController extends Controller
 
         if (!$page) {
             throw $this->createNotFoundException(
-                'No product found for id '. $id
+                'No pages found for id '. $id
             );
         }
 
-        return $this->render('KanCmsBundle:Default:index.html.twig', array(
-            'page' => $page,
-            'created' => $page->getCreated()->format('Y-m-d H:i:s'),
-            'edited' => $page->getEdited()->format('Y-m-d H:i:s'),
-        ));
+        return $this->render('KanCmsBundle:Default:show.html.twig', array(
+                                                                          'page' => $page,
+                                                                          'created' => $page->getCreated()->format('Y-m-d H:i:s'),
+                                                                          'edited' => $page->getEdited()->format('Y-m-d H:i:s'),
+                                                                     ));
     }
 
-    public function createAction()
+    public function createAction(Request $request)
     {
+        // just setup a fresh $task object (remove the dummy data)
         $page = new Page();
-        $title = 'Kan page 1';
-        $description = 'Test page description';
-        $created = $edited = new \DateTime();
 
-        $page->setTitle($title);
-        $page->setDescription($description);
-        $page->setCreated($created);
-        $page->setEdited($edited);
+        $form = $this->createFormBuilder($page)
+            ->add('title', 'text')
+            ->add('description', 'textarea')
+            ->add('created', 'date')
+            ->add('edited', 'date')
+            ->add('save', 'submit')
+            ->getForm();
 
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($page);
-        $em->flush();
+        $form->handleRequest($request);
 
-        return $this->render('KanCmsBundle:Default:create.html.twig', array(
-            'page' => $page,
-            'created' => $page->getCreated()->format('Y-m-d H:i:s'),
-            'edited' => $page->getEdited()->format('Y-m-d H:i:s'),
-        ));
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($page);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('kan_cms_index'));
+        }else{
+            return $this->render('KanCmsBundle:Default:create.html.twig', array(
+                'page_form' => $form->createView(),
+            ));
+        }
+//        $page = new Page();
+//        $title = 'Kan page 1';
+//        $description = 'Test page description';
+//        $created = $edited = new \DateTime();
+//
+//        $page->setTitle($title);
+//        $page->setDescription($description);
+//        $page->setCreated($created);
+//        $page->setEdited($edited);
+//
+//        $em = $this->getDoctrine()->getManager();
+//        $em->persist($page);
+//        $em->flush();
+//
+//        return $this->render('KanCmsBundle:Default:create.html.twig', array(
+//            'page' => $page,
+//            'created' => $page->getCreated()->format('Y-m-d H:i:s'),
+//            'edited' => $page->getEdited()->format('Y-m-d H:i:s'),
+//        ));
     }
 
     public function updateAction($id)
@@ -60,18 +109,47 @@ class DefaultController extends Controller
 
         if (!$page) {
             throw $this->createNotFoundException(
-                'No product found for id '. $id
+                'No page found for id '. $id
             );
         }
 
-        $page->setTitle('updated title');
+        $form = $this->createFormBuilder($page)
+            ->add('title', 'text')
+            ->add('description', 'textarea')
+            ->add('edited', 'date')
+            ->add('save', 'submit')
+            ->getForm();
 
+        $form->handleRequest($this->getRequest());
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($page);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('kan_cms_index'));
+        }else{
+            return $this->render('KanCmsBundle:Default:update.html.twig', array(
+                                                                               'page_form' => $form->createView(),
+                                                                          ));
+        }
+    }
+
+    public function deleteAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $page = $em->getRepository('KanCmsBundle:Page')
+            ->find($id);
+
+        if (!$page) {
+            throw $this->createNotFoundException(
+                'No page found for id '. $id
+            );
+        }
+
+        $em->remove($page);
         $em->flush();
 
-        return $this->render('KanCmsBundle:Default:update.html.twig', array(
-            'page' => $page,
-            'created' => $page->getCreated()->format('Y-m-d H:i:s'),
-            'edited' => $page->getEdited()->format('Y-m-d H:i:s'),
-        ));
+        return $this->redirect($this->generateUrl('kan_cms_index'));
     }
 }
